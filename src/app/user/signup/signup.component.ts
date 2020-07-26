1244661135e762935c21ea47e3d5392db1700e60
saurabh <saurabhbharti9@gmail.com>
2020-07-26 19:34:39 +0530

    added signup component

--- src/app/user/signup/signup.component.ts
+++ src/app/user/signup/signup.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from '../user.service';
+import { ToastConfig, Toaster } from 'ngx-toast-notifications';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
-  styleUrls: ['./signup.component.css']
+  styleUrls: ['./signup.component.css'],
 })
 export class SignupComponent implements OnInit {
+  /**Define fields */
+  public firstName: String;
+  public lastName: String;
+  public email: String;
+  public mobile: String;
+  public password: String;
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
+  ngOnInit(): void {}
+  /**Signup function */
+  public signUpUser(): any {
+    console.log('Signup user');
   }
-
 }
