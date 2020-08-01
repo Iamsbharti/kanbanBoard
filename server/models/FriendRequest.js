7a0ea3f33a3fa8e05b5b973bae021a5b2012744d
saurabh <saurabhbharti9@gmail.com>
2020-08-02 04:10:02 +0530

    added Friend request model

---
+++ server/models/FriendRequest.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose");
+
+const friendRequestSchema = mongoose.Schema({
+  senderID: {
+    type: String,
+    required: true,
+  },
+  senderName: {
+    type: String,
+    required: true,
+  },
+  recieverId: {
+    type: String,
+    required: true,
+  },
+  recieverName: {
+    type: String,
+    required: true,
+  },
+  status: {
+    type: String,
+    default: "pending",
+  },
+  createdOn: {
+    type: Date,
+    default: Date.now(),
+  },
+  modifiedOn: {
+    type: Date,
+  },
+});
+module.exports = mongoose.model("FriendRequest", friendRequestSchema);
